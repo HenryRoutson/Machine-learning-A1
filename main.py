db84b952c4d61cb4a44584e9cb2abba754b0ea0e
Henry Routson <hroutson@student.unimelb.edu.au>
2024-04-05 20:05:07 +1100

    BUGGED

--- main.py
+++ main.py
@@ -5,6 +5,8 @@ import scipy
 import matplotlib.pyplot as plt
 import csv
 import math
+import random
+from collections import Counter
 
 
 # import data
@@ -13,7 +15,7 @@ import math
 TEST_PATH = 'COMP30027_2024_asst1_data/winequality-test.csv'
 TRAIN_PATH = 'COMP30027_2024_asst1_data/winequality-train.csv'
 
-LABELS = [
+ATTRIBUTES = [
     'fixedAcidity',
     'volatileAcidity',
     'citricAcid',
@@ -66,10 +68,44 @@ assert(distance_euclidean([1], [2]) == 1.0)
 
  
 # using loadtxt()
-all_data = np.loadtxt(TRAIN_PATH, delimiter=",", dtype=float, skiprows=1)
+training_data = np.loadtxt(TRAIN_PATH, delimiter=",", dtype=float, skiprows=1)
 
 
-for row in all_data :
-    for row2 in all_data :
-      print(distance_euclidean(row, row2))
+def instance_attributes(instance) :
+  assert(len(instance) == len(ATTRIBUTES))
+  return instance[::-1]
+
+def instance_label(instance) :
+  assert(len(instance) == len(ATTRIBUTES))
+  return instance[-1]
+
+def run_knn_return_label(test_instance : list[float], n : int) -> any :
+
+  most_similar_train_instances = sorted([
+      (
+        distance_euclidean(instance_attributes(train_instance), test_instance),
+        random.randint(1, 1000000), # don't sort based on label
+        instance_label(train_instance)
+      ) 
+      for train_instance in training_data ], 
+    reverse=True
+  )
+
+
+  labels = [label for (distance, random, label) in most_similar_train_instances]
+
+  labels_of_n_closest = labels[:n]
+  most_common_label_of_n_closest = Counter(labels_of_n_closest).most_common()[0][0]
+
+  print(labels_of_n_closest, most_common_label_of_n_closest)
+
+  return most_common_label_of_n_closest
+
+  
+
+
+# test each instance in the test data
+testing_data  = np.loadtxt(TEST_PATH,  delimiter=",", dtype=float, skiprows=1)
+for test_instance in testing_data :
+  run_knn_return_label(test_instance, 9)
 
