bb7d0d269cd0466be8c1a3f3458130dbe06f20cc
Henry Routson <hroutson@student.unimelb.edu.au>
2024-04-05 17:10:30 +1100

    tiny

---
+++ main.py
@@ -0,0 +1,21 @@
+
+import numpy as np
+import sklearn
+import matplotlib.pyplot as plt
+
+
+x = np.linspace(0, 5, 100)
+y = x**2
+plt.plot(x, y)
+plt.xlabel("X-axis")
+plt.ylabel("Y-axis")
+plt.title("graph of $x^2$")
+plt.show()
+
+
+
+
+
+# import data 
+
+# KNN 
